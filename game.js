b02a94672a4dd26ce5a0c10e657428340d5600fb
Aly <aly.hassan9@gmail.com>
2013-02-23 19:42:01 -0500

    Creating circles where user taps/clicks

--- game.js
+++ game.js
@@ -1,3 +1,7 @@
+/**
+ * A simple game where the user presses on randomly appearing food?
+ *  February 2013
+*/
 var canvasElement = document.getElementById('canvas');
     
 var c = canvasElement.getContext('2d');  
@@ -19,9 +23,44 @@ function resizeCanvas() {
 clear();
 
 function clear(){
-    c.fillStyle = '#00ffff';    
+    c.fillStyle = 'LightGreen';    
     c.beginPath();  
     c.rect(0, 0, c.width, c.height);  
     c.closePath();  
     c.fill();
 }
+
+canvasElement.addEventListener("mousedown",getPosition,false);
+
+var loop, w=50;
+
+function getPosition(event){
+    //If we're interrupted, cancel old circle!
+    if(w!=50){
+        w = 50;
+        clear();
+        clearInterval(loop);
+    }
+    
+    var x = event.x;
+    var y = event.y; 
+    x -= canvasElement.offsetLeft;
+    y -= canvasElement.offsetTop;
+    loop = setInterval(function(){drawMouseCircle(x,y)}, 7);
+}
+
+function drawMouseCircle(x, y){
+    clear();
+    c.beginPath();    
+    c.lineWidth = w/7;
+    c.strokeStyle = 'Green';
+    c.arc(x,y,w,0,2*Math.PI,true);
+    c.stroke();
+    w--;
+    if(w===0){
+        w=50;
+        clear();
+        clearInterval(loop);
+    }
+}
+
