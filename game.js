ac125f4b8f879c908c744a19aa454017c0e01043
Aly <aly.hassan9@gmail.com>
2013-02-25 17:11:59 -0500

    Targets show up, array fixed and integrated drawing methods into a single interval

--- game.js
+++ game.js
@@ -4,7 +4,6 @@
  *  February 2013
 */
 var canvasElement = document.getElementById('canvas');
-    
 var c = canvasElement.getContext('2d');  
 c.width = 0;
 c.height = 0;
@@ -14,17 +13,24 @@ window.addEventListener('resize', resizeCanvas, false);
 var target = [], targetCount = 0;
 var POINT = 0,TYPE = 1, WIDTH = 2;
 
-function addTarget(){    
-    var random = Math.random()*c.width; //Random from 0 to width
-    var randomY = Math.random()*c.height; //Random from 0 to height
-    target[targetCount][POINT] = new point(random,randomY); //Add random point
-    random = Math.random(); //Random from 0 to 1
+var gameLoop = setInterval(function(){addTarget()}, 1000);
+var drawLoop = setInterval(function(){drawTargets()}, 6);
+
+function addTarget(){  
+    console.log(targetCount);
+    var intialPoint = new point(Math.random()*c.width, Math.random()*c.height);
+    var random = Math.round(Math.random()); //Random from 0 to 1
     if(random === 0) {
         random--; 
     }
-    target[targetCount][TYPE] = random; //Type: -1 or 1
-    target[targetCount][WIDTH] = 30; //Width
+    target.push([intialPoint,random,30]);
     targetCount++;
+    if(targetCount == 15){
+        clearInterval(gameLoop);
+        clearInterval(drawLoop);
+        drawTargets();
+        alert("Game Over!");
+    }
 }
 
 function checkPoint(x,y){
@@ -58,67 +64,65 @@ clear();
 function clear(){
     c.fillStyle = 'LightGreen';  
     c.rect(0, 0, c.width, c.height);  
-    c.closePath();  
     c.fill();
 }
 
 window.addEventListener("mousedown",getPosition,false);
 
-var loop1, loop2, w=50;
-addTarget();
-drawTargets();
+//var mouseLoop,
+var mouseW=50, mouseX, mouseY, drawMouse = false;
 
 function getPosition(event){
     //If we're interrupted, cancel old circle!
-    if(w!=50){
-        w = 50;
-        clear();
-        clearInterval(loop1);
+    if(mouseW!=50){
+        mouseW = 50;
     }
-    var x = event.x;
-    var y = event.y; 
-    x -= canvasElement.offsetLeft;
-    y -= canvasElement.offsetTop;
-    loop1 = setInterval(function(){drawMouseCircle(x,y)}, 6);
+    mouseX = event.x - canvasElement.offsetLeft;
+    mouseY = event.y - canvasElement.offsetTop;   
+    drawMouse = true;
+    drawTargets();
 }
 
 function drawMouseCircle(x, y){
-//    clear();
     c.beginPath();    
-    c.lineWidth = w/10;
+    c.lineWidth = mouseW/10;
     c.strokeStyle = '#7fb883';
-    c.arc(x,y,w,0,2*Math.PI,true);
-    c.stroke();
-    w--;
-    if(w===0){
-        w=50;
-        clear();
-        clearInterval(loop1);
+    c.arc(x,y,mouseW,0,2*Math.PI,true);
+    c.stroke();  
+    mouseW--;
+    if(mouseW === 0){
+        drawMouse = false;
     }
+    
 }
 
 function drawTargets(){
     clear();
-    alert("t");
-    for(var i = 0; i < targetCount; i++){
-        if(target[i][TYPE] === 1){
+    if(drawMouse){
+        drawMouseCircle(mouseX,mouseY);    
+    }
+    
+    for(var i = 0; i < targetCount; i++){        
+            
+        if(target[i][TYPE] == 1){
             c.fillStyle = '#8DCDEE'; //blue
             c.beginPath();
             c.rect(target[i][POINT].x, target[i][POINT].y, target[i][WIDTH], target[i][WIDTH]);
             c.closePath();
             c.fill();
-        }
-        else{
+        }else{
             c.fillStyle = '#E84343';  //red
             c.beginPath();
-            c.rect(target[i][POINT].x, target[i][POINT].y, target[i][WIDTH], target[targetCount][WIDTH]);
+            c.rect(target[i][POINT].x, target[i][POINT].y, target[i][WIDTH], target[i][WIDTH]);
             c.closePath();
             c.fill();
         }
         
+        c.strokeStyle = '#585858';
+        c.lineWidth = 1;
+        c.rect(target[i][POINT].x, target[i][POINT].y, target[i][WIDTH], target[i][WIDTH]);
+        c.stroke();
+        
     }
     
 }
-
-var speed = 1000;
-//setInterval(addTarget(), speed);
